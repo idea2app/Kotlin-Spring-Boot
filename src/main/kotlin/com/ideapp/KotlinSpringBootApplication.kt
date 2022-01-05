f23f43fcd55907cbc55522b61a7c16cb178cc9d4
mythcsj <mythcsj@163.com>
2022-01-05 14:00:54 +0800

    pom修改

--- src/main/kotlin/com/ideapp/KotlinSpringBootApplication.kt
+++ src/main/kotlin/com/ideapp/KotlinSpringBootApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinSpringBootApplication
+open class KotlinSpringBootApplication
 
 fun main(args: Array<String>) {
 	runApplication<KotlinSpringBootApplication>(*args)
